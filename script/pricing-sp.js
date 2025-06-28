a9035aebb80104869b708114a8e1c10fe06e5369
Matteus Tirado <matteustirado@gmail.com>
2025-06-28 00:53:47 -0300

    Atualizar o pricing-sp.js

--- script/pricing-sp.js
+++ script/pricing-sp.js
@@ -32,9 +32,9 @@ const pricingData = {
         'noite': { player: 53.99, amiga: 107.98, marmita: 161.97 }
     },
     'sexta': {
-        'manha': { player: 29.99, amiga: 35.99, marmita: 49.99 },
-        'tarde': { player: 32.99, amiga: 49.99, marmita: 59.99 },
-        'noite': { player: 35.99, amiga: 54.99, marmita: 69.99 }
+        'manha': { player: 34.99, amiga: 58.99, marmita: 79.99 },
+        'tarde': { player: 49.99, amiga: 79.99, marmita: 89.99 },
+        'noite': { player: 53.99, amiga: 89.99, marmita: 119.99 }
     },
     'sabado': {
         'manha': { player: 34.99, amiga: 58.99, marmita: 79.99 },
