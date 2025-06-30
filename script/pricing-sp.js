9d088d370b0a08f9f9d19db6b9fd3614e5f27fed
Matteus Tirado <matteustirado@gmail.com>
2025-06-30 01:15:48 -0300

    Atualizar o pricing-sp.js

--- script/pricing-sp.js
+++ script/pricing-sp.js
@@ -27,25 +27,25 @@ const pricingData = {
         'noite': { player: 35.99, amiga: 54.99, marmita: 69.99 }
     },
     'quinta': {
-        'manha': { player: 34.99, amiga: 58.99, marmita: 79.99 },
-        'tarde': { player: 49.99, amiga: 99.98, marmita: 149.97 },
-        'noite': { player: 53.99, amiga: 107.98, marmita: 161.97 }
+        'manha': { player: 29.99, amiga: 35.99, marmita: 49.99 },
+        'tarde': { player: 32.99, amiga: 49.99, marmita: 59.99 },
+        'noite': { player: 35.99, amiga: 54.99, marmita: 69.99 }
     },
     'sexta': {
-        'manha': { player: 34.99, amiga: 58.99, marmita: 79.99 },
-        'tarde': { player: 49.99, amiga: 79.99, marmita: 89.99 },
-        'noite': { player: 53.99, amiga: 89.99, marmita: 119.99 }
-    },
+        'manha': { player: 29.99, amiga: 35.99, marmita: 49.99 },
+        'tarde': { player: 32.99, amiga: 49.99, marmita: 59.99 },
+        'noite': { player: 35.99, amiga: 54.99, marmita: 69.99 }
+     },
     'sabado': {
         'manha': { player: 34.99, amiga: 58.99, marmita: 79.99 },
         'tarde': { player: 49.99, amiga: 79.99, marmita: 89.99 },
         'noite': { player: 53.99, amiga: 89.99, marmita: 119.99 }
     },
     'domingo': {
-        'manha': { player: 34.99, amiga: 58.99, marmita: 79.99 },
-        'tarde': { player: 49.99, amiga: 79.99, marmita: 89.99 },
-        'noite': { player: 53.99, amiga: 89.99, marmita: 119.99 }
-    },
+        'manha': { player: 29.99, amiga: 35.99, marmita: 49.99 },
+        'tarde': { player: 32.99, amiga: 49.99, marmita: 59.99 },
+        'noite': { player: 35.99, amiga: 54.99, marmita: 69.99 }
+     },
     'feriados': {
         'manha': { player: 34.99, amiga: 58.99, marmita: 79.99 },
         'tarde': { player: 49.99, amiga: 79.99, marmita: 89.99 },
